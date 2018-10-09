8a5c265e333ccfe4e708326ae9bd4b05bacca006
John Maguire <jmaguire@mdsol.com>
2018-10-09 12:04:58 -0400

    converted get_location.js to react component
    added new component for getting location
    
    added event listener for search autocomplete
    
    added new input location component
    
    alphabetize registration.js imports

--- client/app/startup/registration.js
+++ client/app/startup/registration.js
@@ -6,39 +6,41 @@
 import ReactOnRails from 'react-on-rails';
 import { setup } from '../libs/i18n/setup';
 import '../styles/_global.scss';
+import { Accordion } from '../components/Accordion';
 import { Avatar } from '../components/Avatar';
 import { Chart } from '../components/Chart';
 import { ChartControl } from '../components/Chart/ChartControl';
+import { Form } from '../components/Form';
 import { Header } from '../components/Header';
 import { HeaderProfile } from '../components/Header/HeaderProfile';
-import { Resource } from '../components/Resource';
-import { Tag } from '../components/Tag';
+import { InputLocation } from '../components/Input/InputLocation';
 import { Logo } from '../components/Logo';
+import { Modal } from '../components/Modal';
+import { Notifications } from '../widgets/Notifications';
+import { Resource } from '../components/Resource';
+import { Resources } from '../widgets/Resources';
 import { Story } from '../components/Story';
 import { StoryDraft } from '../components/Story/StoryDraft';
 import { StoryActions } from '../components/Story/StoryActions';
 import { StoryCategories } from '../components/Story/StoryCategories';
 import { StoryMoods } from '../components/Story/StoryMoods';
 import { StoryDate } from '../components/Story/StoryDate';
+import { Tag } from '../components/Tag';
 import { Tooltip } from '../components/Tooltip';
-import { Modal } from '../components/Modal';
-import { Form } from '../components/Form';
-import { Accordion } from '../components/Accordion';
-import { Resources } from '../widgets/Resources';
-import { Notifications } from '../widgets/Notifications';
 import { ToggleLocale } from '../widgets/ToggleLocale';
 
 setup();
 
 // This is how react_on_rails can see the Components in the browser.
 ReactOnRails.register({
-  Avatar,
   Accordion,
+  Avatar,
   Chart,
   ChartControl,
   Form,
   Header,
   HeaderProfile,
+  InputLocation,
   Logo,
   Modal,
   Notifications,
